851d2a06adf3873517d955b210f0792f73687a79
NakatsuboYusuke <>
2020-01-11 21:54:01 +0900

    study chapter3 section14 on the way

--- chapter3/main_form.js
+++ chapter3/main_form.js
@@ -2,7 +2,20 @@ var app = new Vue({
   el: '#app',
   data: {
     // 初期値をセット
-    message: 'Hello, World!',
-    val: true
+    // message: 'Hello, World!',
+    // val: true
+    // val: []
+    // val: ''
+    preview: '',
+    val: 50
+  },
+  methods: {
+    handleChange: function(event) {
+      var file = event.target.files[0]
+      console.log(file)
+      if (file && file.type.match(/^image\/(png|jpeg)$/)) {
+        this.preview = window.URL.createObjectURL(file)
+      }
+    }
   }
 })
