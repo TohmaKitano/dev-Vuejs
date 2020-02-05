076df4f83e07495eac96ee251626b79a803c5c4b
NakatsuboYusuke <>
2020-02-05 13:52:53 +0900

    study Chapter8 section43 on the way

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -4,13 +4,52 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 // ストアはアプリケーション内に作った、仮想のデータベースのようなもの。
+// const store = new Vuex.Store({
+//   state: {
+//     count: 0
+//   },
+//   mutations: {
+//     increment(state) {
+//       state.count++
+//     }
+//   }
+// })
+// export default store
+
 const store = new Vuex.Store({
   state: {
-    count: 0
+    count: 0,
+    list: [
+      { id: 1, name: 'りんご', price: 100 },
+      { id: 2, name: 'ばなな', price: 200 },
+      { id: 3, name: 'いちご', price: 300 }
+    ]
+    // message: 'Hello, Vue.js'
   },
-  mutations: {
-    increment(state) {
-      state.count++
+  // mutations: {
+  //   increment(state) {
+  //     state.count++
+  //   }
+  // },
+  getters: {
+    // ステートを返す
+    count(state, getters, rootState, rootGetter) {
+      return state.count
+    },
+    // リストの中からpriceプロパティの最大値を返す
+    max(state) {
+      return state.list.reduce((a, b) => {
+        return a > b.price ? a : b.price
+      }, 0)
+    },
+    // 引数付きゲッター
+    item(state) {
+      // 引数を使用するためアロー関数を返している
+      return id => state.list.find(el => el.id === id)
+    },
+    // 別のゲッターを使う
+    name(state, getters) {
+      return id => getters.item(id).name
     }
   }
 })
